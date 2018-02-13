4a7ac1fc40dcca27edff14622f2be8368363eec0
manuelruder <manuelruder@users.noreply.github.com>
2018-02-13 09:48:07 +0100

    Update download_models.sh
    Removed duplicate entry, added The Scream model

--- models/download_models.sh
+++ models/download_models.sh
@@ -6,14 +6,14 @@ ROOT_URL=https://lmb.informatik.uni-freiburg.de/data/fast-artistic-videos/models
 wget $ROOT_URL/checkpoint-mosaic-video.t7
 wget $ROOT_URL/checkpoint-picasso-video.t7
 wget $ROOT_URL/checkpoint-schlief-video.t7
-wget $ROOT_URL/checkpoint-mosaic-video.t7
+wget $ROOT_URL/checkpoint-scream-video.t7
 wget $ROOT_URL/checkpoint-WomanHat-video.t7
 wget $ROOT_URL/checkpoint-candy-video.t7
 # Image models
 wget $ROOT_URL/checkpoint-mosaic-image.t7
 wget $ROOT_URL/checkpoint-picasso-image.t7
 wget $ROOT_URL/checkpoint-schlief-image.t7
-wget $ROOT_URL/checkpoint-mosaic-image.t7
+wget $ROOT_URL/checkpoint-scream-image.t7
 wget $ROOT_URL/checkpoint-WomanHat-image.t7
 wget $ROOT_URL/checkpoint-candy-image.t7
 cd ..
